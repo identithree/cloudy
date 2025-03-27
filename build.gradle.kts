07640e62a1e5b5ccdffd96d09c4786a94fe7f891
Quinn Lane <laneq@wit.edu>
2025-03-27 14:59:38 -0400

    :building_construction: Use Gradle best practices
     - Use Gradle's version catalog for dependency management (gradle/libs.versions.toml)
     - Programmatically create version string and embed in runtime
     - Define configurable settings in gradle.properties
     - Configure Java plugin
     - Add ShadowJAR build support
     - Add Log4J as a dependency
     - Configure various Gradle tasks
    
    Signed-off-by: Quinn Lane <laneq@wit.edu>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,80 @@
+// Cloudy - build.gradle.kts
+// Written by Quinn Lane - https://quinnlane.dev
+
+// Imports (for development versioning)
+import java.net.InetAddress
+import java.net.UnknownHostException
+import java.text.SimpleDateFormat
+import java.util.*
+
+// Plugin Definition
+// Plugins, like dependencies, are defined in gradle/libs.versions.toml
 plugins {
-    id("java")
-    id("application")
-    id("org.openjfx.javafxplugin") version "0.1.0"
+    alias(libs.plugins.java) // Java
+	alias(libs.plugins.javafx) // JavaFX
+	alias(libs.plugins.application) // Application
+	alias(libs.plugins.shadow) // Shadow
 }
 
-group = "dev.quinnlane"
-version = "1.0-SNAPSHOT"
+// Version String Creator
+fun createVersionString(): String {
+	// Create a StringBuilder for the version
+	val versionString = StringBuilder()
 
+	// Get the root version string from gradle.properties (should be in SemVer)
+	val versionRoot = (project.properties["version"]!! as String)
+	// Check if this is a release build
+	val isRelease = (project.properties["buildForRelease"]!! as String).toBoolean()
+	// Check if this is a CI build
+	val isCI = System.getenv("CI")
+
+	// Add version root to output
+	versionString.append(versionRoot.substringBefore("-"))
+
+	// Check if the build is a release build
+	if (!isRelease) {
+		// Append snapshot tag
+		versionString.append("-SNAPSHOT")
+
+		// Check if this is a CI build
+		if (isCI != null && isCI.toBoolean()) {
+			// If it is, then append shortened commit SHA to the version
+			val buildSha = System.getenv("GITHUB_SHA").substring(0, 7)
+			versionString.append("+rev.").append(buildSha)
+		} else {
+			// If it is not, then append the building computer's hostname
+			var hostname: String
+
+			try {
+				hostname = InetAddress.getLocalHost().hostName.substringBefore(".")
+			} catch (_: UnknownHostException) {
+				hostname = "unknown"
+			}
+
+			versionString.append("+local.").append(hostname)
+		}
+	} else {
+		// If this is a release build, put back the removed data
+		versionString.append("-").append(versionRoot.substringAfter("-", ""))
+	}
+
+	// Return final version string
+	return versionString.toString()
+}
+
+// Variable Definition
+version = createVersionString() // Define Version
+group = project.properties["mavenGroup"]!! as String // Define Group
+val archivesBaseName = project.properties["archivesBaseName"]!! as String // Define archive base name (project name)
+val licenseFile = rootProject.properties["licenseFile"]!! as String // Define LICENSE file
+val javaTargetVersion = (rootProject.properties["javaTargetVersion"]!! as String).toInt() // Define Java target version
+
+// Application Configuration
 application {
-    mainClass = "dev.quinnlane.cloudy.Main"
+    mainClass = "${group}.${archivesBaseName}.Main"
 }
 
+// Maven Repositories (for dependency resolution, not publishing)
 repositories {
 	// Maven Central Repository
     maven {
@@ -18,21 +82,85 @@ repositories {
     }
 }
 
+// Java Configuration
+java {
+	// Required for IDEs like Eclipse and Visual Studio Code
+	sourceCompatibility = JavaVersion.toVersion(javaTargetVersion)
+	targetCompatibility = JavaVersion.toVersion(javaTargetVersion)
+
+	// Include sources in the build
+	withSourcesJar()
+
+	// Do not infer a module path
+	modularity.inferModulePath.set(false)
+}
+
+// JavaFX Plugin
+// As JavaFX is considered a library, it's version is defined in gradle/libs.version.toml
 javafx {
-    version = "24"
+    version = libs.versions.javafx.asProvider().get()
     modules = listOf("javafx.controls", "javafx.fxml")
 }
 
+// Dependency Definitions
+// All the dependencies are declared at gradle/libs.version.toml and referenced with "libs.<id>"
+// See https://docs.gradle.org/current/userguide/platforms.html for information on how version catalogs work.
 dependencies {
 	// Playwright
-	implementation("com.microsoft.playwright:playwright:1.51.0")
+	implementation(libs.playwright)
 	// JetBrains Annotations
-	implementation("org.jetbrains:annotations:26.0.2")
+	implementation(libs.jetbrains.annotations)
+	// Log4J (at runtime only)
+	implementation(libs.bundles.log4j)
 	// JUnit (in test module only)
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(libs.junit.jupiter)
+	testRuntimeOnly(libs.junit.platform.launcher)
 }
 
-tasks.test {
-    useJUnitPlatform()
+// Gradle Task Configuration
+tasks {
+	processResources {
+		inputs.property("version", project.version)
+		filteringCharset = "UTF-8"
+		filesMatching("gradle/version.properties") {
+			expand("version" to project.version)
+		}
+	}
+
+	// Set Java compiler options
+	withType(JavaCompile::class.java).configureEach {
+		options.release.set(javaTargetVersion)
+		options.encoding = "UTF-8"
+	}
+
+	// Configure JAR building
+	jar {
+		// Configure MANIFEST.MF
+		manifest {
+			attributes["Main-Class"] = application.mainClass
+			attributes["Implementation-Title"] = "Cloudy"
+			attributes["Implementation-Version"] = project.version
+			attributes["Implementation-Build-Date"] = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US).format(Calendar.getInstance().time)
+		}
+
+		// Rename the license file to include the archive name
+		from(licenseFile) {
+			rename { "${it}_${archivesBaseName}" }
+		}
+	}
+
+	named("shadowJar", com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
+		archiveClassifier = "shadow"
+		minimize()
+	}
+
+	// Unit Testing
+	test {
+		// Use JUnit
+		useJUnitPlatform()
+		// Logging Options
+		testLogging {
+			events("passed", "skipped", "failed")
+		}
+	}
 }
--- src/main/java/dev/quinnlane/cloudy/Main.java
+++ src/main/java/dev/quinnlane/cloudy/Main.java
@@ -1,15 +1,48 @@
 package dev.quinnlane.cloudy;
 
+import dev.quinnlane.cloudy.backend.Backend;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
+import java.util.Properties;
 
 public class Main extends Application {
+	/**
+	 * Initializes a new instance of Log4J for use during execution
+	 */
+	public static final Logger logger = LogManager.getFormatterLogger("Cloudy");
+	/**
+	 * Stores the current version of the application
+	 */
+	public static final String VERSION;
+
+	// Get the program version from version.propeerties
+	static {
+		String version;
+		Properties props = new Properties();
+
+		try (InputStream is = Main.class.getResourceAsStream("/gradle/version.properties")) {
+			props.load(is);
+			version = props.getProperty("version");
+		} catch (IOException e) {
+			logger.error("Failed to load version.properties file!", e);
+			version = "X.X.X";
+		}
+
+		VERSION = version;
+	}
+
     public static void main(String[] args) {
+		logger.info("Starting Cloudy...");
+		// Launch JavaFX
         launch(args);
     }
 
@@ -17,8 +50,9 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("views/MainView.fxml")));
 
-        stage.setTitle("Cloudy");
+        stage.setTitle("Cloudy - v" + VERSION);
         stage.setScene(new Scene(root));
         stage.show();
+		logger.info("Cloudy has been started!");
     }
 }
