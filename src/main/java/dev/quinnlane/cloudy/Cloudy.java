68797dec14a52e637cd6ee9f7bd630afbda5bad7
Quinn Lane <laneq@wit.edu>
2025-04-02 21:46:33 -0400

    :art: Refactor initialization logic and enhance logging.
    Introduced clearer lifecycle management for backend/frontend instances and added detailed logging for better traceability. Centralized version handling and improved initialization robustness, ensuring descriptive logs during application startup.
    
    Signed-off-by: Quinn Lane <laneq@wit.edu>

--- src/main/java/dev/quinnlane/cloudy/Cloudy.java
+++ src/main/java/dev/quinnlane/cloudy/Cloudy.java
@@ -1,5 +1,6 @@
 package dev.quinnlane.cloudy;
 
+import dev.quinnlane.cloudy.backend.Backend;
 import dev.quinnlane.cloudy.frontend.Frontend;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,32 +12,84 @@ import java.util.Properties;
 public class Cloudy {
 	/**
 	 * Initializes a new instance of Log4J for use during execution
+	 * This field stores the arguments that Cloudy was executed with.
+	 */
+	private static String[] arguments;
+
+	/**
+	 * This field stores the instance of the Backend that Cloudy uses during runtime.
+	 */
+	private static Frontend frontend;
+
+	/**
+	 * THis field stores the version of Cloudy that is currently running.
+	 */
+	private static String VERSION = "X.X.X";
+
+	/**
+	 * Initializes a new instance of Log4J for use across this entrypoint class and all common classes.
+	 */
+	protected static final Logger logger = LogManager.getFormatterLogger("Cloudy > Common");
+
+	/**
+	 *
 	 */
 	public static final Logger logger = LogManager.getFormatterLogger("Cloudy");
 	/**
-	 * Stores the current version of the application
+	 * Retrieves the instance of the Frontend used during runtime.
+	 *
+	 * @return the current Frontend instance
 	 */
-	public static final String VERSION;
+	public static Frontend getFrontend() {
+		return frontend;
+	}
 
-	// Get the program version from version.propeerties
-	static {
-		String version;
+	/**
+	 * Retrieves the arguments that the program was executed with.
+	 *
+	 * @return an array of Strings representing the arguments passed on program startup
+	 */
+	public static String[] getArguments() {
+		return arguments;
+	}
+
+	/**
+	 * Retrieves the version of the Cloudy application that is currently running.
+	 *
+	 * @return a String representing the current version of the application
+	 */
+	public static String getVersion() {
+		return VERSION;
+	}
+
+	// Entrypoint method
+    public static void main(String[] args) {
+		// -- Load the program version at runtime from version.properties
+		logger.debug("Loading version from version.properties file...");
+		String version = VERSION;
 		Properties props = new Properties();
 
 		try (InputStream is = Cloudy.class.getResourceAsStream("/gradle/version.properties")) {
 			props.load(is);
 			version = props.getProperty("version");
+			logger.trace("version.properties file reports version as %s", version);
 		} catch (IOException e) {
 			logger.error("Failed to load version.properties file!", e);
-			version = "X.X.X";
+			logger.info("Version information could not be loaded. The interface might look weird, but the app is completely functional in this state.");
 		}
 
 		VERSION = version;
-	}
+
+		// -- Start Cloudy
+		logger.info("Starting Cloudy v%s...", VERSION);
+		arguments = args;
 
     public static void main(String[] args) {
 		logger.info("Starting Cloudy...");
 		Frontend.launch(Frontend.class, args);
+		// Initialize backend and frontend
+		frontend = new Frontend();
+
 		logger.info("Cloudy has been started!");
     }
 }
--- src/main/java/dev/quinnlane/cloudy/frontend/Frontend.java
+++ src/main/java/dev/quinnlane/cloudy/frontend/Frontend.java
@@ -6,15 +6,32 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Objects;
 
 public class Frontend extends Application {
+	protected static final String TITLE = "Cloudy - v" + Cloudy.getVersion();
+	protected static final Logger logger = LogManager.getFormatterLogger("Cloudy > Frontend");
+
+	public Frontend() {
+		logger.trace("Frontend class constructor called from thread '%s'", Thread.currentThread().getName());
+		if (Cloudy.getFrontend() != null) {
+			logger.debug("The frontend has already been initialized! Skipping constructor logic...");
+			return;
+		}
+
+		logger.info("Initializing frontend...");
+		new Thread(() -> launch(Cloudy.getArguments()), "Cloudy JavaFX Initializer").start();
+		logger.info("Frontend has been initialized!");
+	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("views/MainView.fxml")));
 
-		stage.setTitle("Cloudy - v" + Cloudy.VERSION);
+		stage.setTitle(TITLE);
 		stage.setScene(new Scene(root));
 		stage.show();
 	}
