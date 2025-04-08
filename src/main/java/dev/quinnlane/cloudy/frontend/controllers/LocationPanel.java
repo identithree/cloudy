18fb9dbe9a30ea14695744edea1277efd737300e
Xavier McClurkin <104314713+gitignore-xav@users.noreply.github.com>
2025-04-08 16:20:03 -0400

    Created LocationPanel and moved things to components folder

---
+++ src/main/java/dev/quinnlane/cloudy/frontend/controllers/LocationPanel.java
@@ -0,0 +1,7 @@
+package dev.quinnlane.cloudy.frontend.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class LocationPanel extends HBox { }
--- src/main/java/dev/quinnlane/cloudy/frontend/controllers/WindPanel.java
+++ src/main/java/dev/quinnlane/cloudy/frontend/controllers/WindPanel.java
@@ -1,4 +1,11 @@
 package dev.quinnlane.cloudy.frontend.controllers;
 
-public class WindPanel {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+
+public class WindPanel extends VBox {
+	@FXML private Label speedText;
+	@FXML private Label compassText;
 }
