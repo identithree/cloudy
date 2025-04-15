0c70125ee169e2120437ae38f4feebcdd4bf3bef
Xavier McClurkin <xavmcc3@gmail.com>
2025-04-15 01:49:06 -0400

    Loaded the mock data provider into the frontend and allowed it to interact with the main view

--- src/main/java/dev/quinnlane/cloudy/frontend/Frontend.java
+++ src/main/java/dev/quinnlane/cloudy/frontend/Frontend.java
@@ -1,6 +1,7 @@
 package dev.quinnlane.cloudy.frontend;
 
 import dev.quinnlane.cloudy.Cloudy;
+import dev.quinnlane.cloudy.frontend.controllers.MainView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,6 +15,17 @@ import java.util.Objects;
 public class Frontend extends Application {
 	private static final String TITLE = "Cloudy - v" + Cloudy.getVersion();
 	public static final Logger logger = LogManager.getFormatterLogger("Cloudy > Frontend");
+
+	private static MainView mainViewController;
+
+	public static MainView getMainViewController() {
+		return mainViewController;
+	}
+
+	private static MainView setMainViewController(MainView mainViewController) {
+		Frontend.mainViewController = mainViewController;
+		return mainViewController;
+	}
 
 	public Frontend() {
 		logger.trace("Frontend class constructor called from thread '%s'", Thread.currentThread().getName());
@@ -29,10 +41,24 @@ public class Frontend extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("views/MainView.fxml")));
+		FXMLLoader mainViewLoader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("views/MainView.fxml")));
+		Parent root = mainViewLoader.load();
+
+		setMainViewController(mainViewLoader.getController());
 
 		stage.setTitle(TITLE);
 		stage.setScene(new Scene(root));
 		stage.show();
+
+		/* TODO
+		    add a tick that runs periodially checking how recent the data is;
+		    if any of the data has changed, update the screen
+		   */
+		updateMainView();
+	}
+
+	private void updateMainView() {
+		if (mainViewController == null) throw new IllegalStateException("MainView has not been initialized!");
+		mainViewController.updateFromBackend(Cloudy.getBackend().getProvider());
 	}
 }
--- src/main/java/dev/quinnlane/cloudy/frontend/controllers/MainView.java
+++ src/main/java/dev/quinnlane/cloudy/frontend/controllers/MainView.java
@@ -1,20 +1,34 @@
 package dev.quinnlane.cloudy.frontend.controllers;
 
+import dev.quinnlane.cloudy.backend.dataproviders.DataProvider;
 import dev.quinnlane.cloudy.common.unit.UnitLocales;
-import dev.quinnlane.cloudy.common.unit.units.Temperature;
+import dev.quinnlane.cloudy.common.unit.units.Speed;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
-public class MainView {
+public class MainView extends AnchorPane {
 	@FXML
 	public TemperaturePanel temperaturePanelController;
+	@FXML
+	public WindPanel windPanelController;
+	@FXML
+	public SunsetPill sunsetPillController;
+	@FXML
+	public OverTimePanel overTimePanelController;
+	@FXML
+	public LocationPanel locationPanelController;
+	@FXML
+	public HumidityPill humidityPillController;
 
 	@FXML
 	public void initialize() {
-		updateFromBackend();
 	}
 
-	public void updateFromBackend() {
+	public void updateFromBackend(DataProvider provider) {
 		// TODO: Fetch temperature from call `Cloudy.getBackend().getProvider().getCurrentTemperature()`.
-		temperaturePanelController.setTemperatureRead(new Temperature(6.0, UnitLocales.CUSTOMARY));
+		temperaturePanelController.setTemperatureRead(provider.getCurrentTemperature());
+
+//		var wind = Cloudy.getBackend().getProvider().getWind();
+		windPanelController.setSpeed(new Speed(10000, UnitLocales.CUSTOMARY));
 	}
 }
